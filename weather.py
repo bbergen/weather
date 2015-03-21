8fbb732e84682bdc74f7fb7250789e69cd98055c
bbergen <bryanbergen@gmail.com>
2015-03-20 21:27:30 -0700

    actual JSON object loaded, printed

--- weather.py
+++ weather.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python2
 import urllib2
 import pygeoip
+import json
 
 def get_json(lat, long):
     weather_url = "http://api.openweathermap.org/data/2.5/weather?" \
             + "lat=" + str(lat) \
             + "&lon=" + str(long)
     response = urllib2.urlopen(weather_url).read()
-    return response
+    return json.loads(response)
 
 def get_ip():
     return urllib2.urlopen('http://ip.42.pl/raw').read()
@@ -22,7 +23,9 @@ def main():
     record = get_location_record(ip)
     lat = record['latitude']
     long = record['longitude']
-    print get_json(lat, long)
+    response = get_json(lat, long)
+    for key in response:
+        print str(response[key])
 
 if __name__ == "__main__":
     main()
