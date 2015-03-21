5ba943f0747dbb7bc5dd338ec753d49ca64cdcc1
bbergen <bryanbergen@gmail.com>
2015-03-20 21:58:00 -0700

    pretty print json

--- weather.py
+++ weather.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python2
+from datetime import date
+from pprint import pprint
+import datetime
 import urllib2
 import pygeoip
 import json
@@ -17,15 +20,19 @@ def get_location_record(ip):
     geo_con = pygeoip.GeoIP('/opt/GeoIP/GeoIP.dat')
     return geo_con.record_by_addr(ip)
 
+def print_weather(json_result):
+    print "Weather for " + str(date.today())
+    print "Current Time: " \
+            + datetime.datetime.now().strftime('%H:%M:%S')
+    pprint(json_result)
+
 def main():
     ip = get_ip()
     print str(ip)
     record = get_location_record(ip)
     lat = record['latitude']
     long = record['longitude']
-    response = get_json(lat, long)
-    for key in response:
-        print str(response[key])
+    print_weather(get_json(lat, long))
 
 if __name__ == "__main__":
     main()
