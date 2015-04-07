edb215bf54a93d0e134419259b2e60155cd9475e
Bryan Bergen <bryanbergen@gmail.com>
2015-04-07 11:18:45 -0700

    add option parsing logic

--- weather.py
+++ weather.py
@@ -4,6 +4,7 @@ from datetime import date
 from pprint import pprint
 from PIL import Image
 from bisect import bisect
+import PIL.ImageOps
 import random
 import datetime
 import urllib2
@@ -72,11 +73,12 @@ def ascii_image(i):
 """ Queries weather server with lat, long
 :param lat: latitude of weather query target
 :param long: longitude of weather query target
+:param days: number of days (1-5) of forecast
 :returns: a JSON object containing weather data for target
 """
-def get_json(lat, long):
+def get_weather(lat, long, days):
     location = "q=" + str(lat) + "," + str(long)
-    weather_url = PREFIX + location + FORMAT + "&num_of_days=1" + KEY
+    weather_url = PREFIX + location + FORMAT + "&num_of_days=" + str(days) + KEY
     response = urllib2.urlopen(weather_url).read()
     return json.loads(response)
 
@@ -108,14 +110,15 @@ def print_weather(json_result):
     print current['weatherIconUrl'][0]['value']
     image = get_image(current['weatherIconUrl'][0]['value'])
     print ascii_image(image)
+    pprint(json_result)
 
-""" Parses command line options
-:returns: map of command line options to values
+""" Parses command line options and handles errors
+:returns: An options object containing commandline arguments
 """
 def parse_options():
     parser = optparse.OptionParser('usage%prog ' + \
-        '[-c <city>] [-z <zipcode>] ' + \
-        '[-p <postal code>] [-f <days of forecast>]')
+        '[-c <city> | -z <zipcode> | ' + \
+        '-p <postal code>] [-f <days of forecast>]')
     parser.add_option('-c', dest='city', type='string', \
         help='target for remote forecast')
     parser.add_option('-z', dest='zip', type='string', \
@@ -125,16 +128,39 @@ def parse_options():
     parser.add_option('-f', dest='days', type='int', \
         help='quantity of future forecasting. max 5')
     (options, args) = parser.parse_args()
+    c = options.city == None
+    z = options.zip == None
+    p = options.pc == None
+    f = options.days == None
+    if not f and (options.days > 5 or options.days < 1):
+      print "Error: Forecast must be between 1 and 5 days"
+      parser.print_usage()
+      exit()
+    if not c and not z:
+      parser.print_usage()
+      exit()
+    elif not c and not p:
+      parser.print_usage()
+      exit()
+    elif not z and not p:
+      parser.print_usage()
+      exit()
+    else:
+      return options
 
 def main():
-    parse_options()
+    options = parse_options()
+    print options
     ip = get_ip()
     print Font.RED + str(ip) + Font.END
     print Font.YELLOW + "Hello World" + Font.END
     record = get_location_record(ip)
+    print str(record['city'])
     lat = record['latitude']
     long = record['longitude']
-    print_weather(get_json(lat, long))
+    days = options.days if options.days != None else 1 
+    weather = get_weather(lat, long, days)
+    print_weather(weather)
 
 if __name__ == "__main__":
     main()
